9c5c192872b5fc8c538ee96cd8d9adff39ddfd10
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-05-12 23:27:17 +0530

    create backend api

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,7 +1,12 @@
 const expreess = require('express');
+const bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
 
 const app = expreess();
+app.use(bodyParser.json());
+
+var employeeController = require('./controllers/employeeController');
 
 const db = require('./config/keys').mongoURI;
 
@@ -15,3 +20,5 @@ app.get('/', (req, res) => res.send('hello'));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
+
+app.use('/employees', employeeController);
