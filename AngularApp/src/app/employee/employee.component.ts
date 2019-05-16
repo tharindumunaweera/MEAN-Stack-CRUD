e1c28509b00c8e88c161a6d2f0fa72bf52ecb76b
Tharindu Munaweera <tharindumunaweera94@gmail.com>
2019-05-16 10:26:02 +0530

    set employee service to employee html

--- AngularApp/src/app/employee/employee.component.ts
+++ AngularApp/src/app/employee/employee.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EmployeeService } from '../shared/employee.service';
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
+  providers: [EmployeeService]
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
   }
--- AngularApp/src/app/shared/employee.service.ts
+++ AngularApp/src/app/shared/employee.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+import { Employee } from './employee.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+
+  selectedEmployee: Employee;
+  employees: Employee[];
 
   constructor() { }
 }
